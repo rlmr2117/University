d5d37aa9531da4850306882d70d4d2d7670095a2
taeuk <rlmr2117@naver.com>
2023-04-10 17:13:09 +0900

    a

--- University/euclid.c
+++ University/euclid.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int Euclid(int a, int b)
+int Euclid(int a, int b) 
 {
-    if (b == 0) return a;//b가 0이 됐을 경우 a 리턴
+    if (b == 0) return a;
 
-    return Euclid(b, a % b);//b가 0이 아닐 경우에 b와 a를 b로 나눈 나머지로 재귀호출
+    return Euclid(b, a % b);
 }
 
-int Euclid2(int x, int y) {
+int Euclid2(int x, int y) { 
     int temp;
 
     while (y != 0) {
